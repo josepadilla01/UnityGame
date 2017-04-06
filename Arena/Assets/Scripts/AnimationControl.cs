a7e096465ef44451ef18aef379bfbf4595e8e426
josepadilla01 <wichopadillan@hotmail.com>
2017-04-05 23:35:43 -0500

    Deleted unnecessary scripts
    Renamed PlayerControl script to AnimationControl

---
+++ Arena/Assets/Scripts/AnimationControl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationControl : MonoBehaviour {
+
+    public Animator anim;
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.G))
+            anim.SetBool("attack", true);
+        else
+            anim.SetBool("attack", false);
+	}
+}
